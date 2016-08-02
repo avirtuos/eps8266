41b8babc07cccd114286d38a215884283da7e12b
Anthony Virtuoso <avirtuos@gmail.com>
2016-08-01 21:52:56 -0400

    Refactored the init and the ThingSpeak files a bit, the new approach reduce power usage by as much as 15%. Its still not perfect though.

--- scripts/dht11_thinkspeak.lua
+++ scripts/dht11_thinkspeak.lua
@@ -1,56 +1,52 @@
 sensor_pin = 1
 
-function publish(temperature, humidity)
+temperature = -1000
+humidity = -1000
 
-    if temperature == -1000 or humidity == -1000 then
-        print("Invalid data")
-        return
-    end
-    
-    url = string.format("http://api.thingspeak.com/update?api_key=API_KEY&field1=%d&field2=%d",
-    temperature, humidity)
-    http.get(url, nil, function(code, data)
-        if (code < 0) then
-          print("HTTP request failed")
-        else
-          print("Success...", code, data)
-        end
-      end)
+status, temp, humi, temp_dec, humi_dec = dht.read(sensor_pin)
+if status == dht.OK then
+    temp = (temp * (9/5)) + 32;
+    print(string.format("T%f;H:%f\r\n",temp,humi))
+    temperature = temp
+    humidity = humi
+elseif status == dht.ERROR_CHECKSUM then
+    print( "DHT Checksum error." )
+elseif status == dht.ERROR_TIMEOUT then
+    print( "DHT timed out." )
 end
 
+wifi.sta.config("wifi_ssd","wifi_pwd")
+wifi.sta.connect()
 
-
-local cnt = 0
-local temperature = -1000
-local humidity = -1000
+local count = 0
+local inProgress = nil
 tmr.alarm(0, 1000, 1, function() 
-
-    --We loop 12 times and read the sensor because the sensor can give weird results initially, seems to have a 'warm up issue'
-    status, temp, humi, temp_dec, humi_dec = dht.read(sensor_pin)
-    if status == dht.OK then
-        print(string.format("%d T:%d;H:%d\r\n",
-              cnt,
-              temp,
-              humi
-        ))
-
-        temperature = temp
-        humidity = humi
-    elseif status == dht.ERROR_CHECKSUM then
-        print( "DHT Checksum error." )
-    elseif status == dht.ERROR_TIMEOUT then
-        print( "DHT timed out." )
-    end
-
-    if(cnt == 8) then
-        publish(temperature, humidity)
-    end
-    
-    cnt = cnt + 1
-    if(cnt > 12) then
-         tmr.stop(0);
-         print("Script done, Going to deep sleep for "..(300).." seconds")
-         node.dsleep(300000000)   
+    count = count + 1
+    if (wifi.sta.getip() == nil) and (count < 30) then 
+        print("Waiting on IP...")
+    elseif(wifi.sta.getip() ~= nil and inProgress == nil) then
+        url = string.format("http://api.thingspeak.com/update?api_key=API_KEY&field1=%f&field2=%f",temperature, humidity)
+        print(url)
+        inProgress = 1
+        http.get(url, nil, function(code, data)
+            if (code < 0) then
+                print("HTTP request failed")
+                inProgress = -1
+            else
+                print("Success...", code, data)
+                inProgress = -1
+            end
+        end)
+    elseif(wifi.sta.getip() ~= nil and inProgress == 1 and count < 30) then
+        print("Waiting on HTTP call to complete")
+    elseif(wifi.sta.getip() ~= nil and inProgress == -1) then
+        tmr.stop(0)
+        print("Script done, Going to deep sleep for "..(300).." seconds")
+        node.dsleep(300000000)   
+    else
+        tmr.stop(0)
+        print("Script failed, Going to deep sleep for "..(300).." seconds")
+        node.dsleep(300000000)   
     end
 end)
 
--- scripts/init.lua
+++ scripts/init.lua
@@ -1,48 +1,12 @@
-print("Ready to start soft ap AND station")
-
-wifi.setmode(wifi.STATIONAP)
-
-local cfg={}
-cfg.ssid="EPS8266_SSID";
-cfg.pwd="EPS8266_PWD"
-wifi.ap.config(cfg)
-cfg={}
-cfg.ip="192.168.2.1";
-cfg.netmask="255.255.255.0";
-cfg.gateway="192.168.2.1";
-wifi.ap.setip(cfg);
-dhcp_config ={}
-dhcp_config.start = "192.168.2.100"
-wifi.ap.dhcp.config(dhcp_config)
-wifi.ap.dhcp.start()
-
-    wifi.sta.config("YOUR_WIFI_SSID","YOUR_WIFI_PWD")
-    wifi.sta.connect()
-    
-    local cnt = 0
-    --gpio.mode(0,gpio.OUTPUT);
-    tmr.alarm(0, 1000, 1, function() 
-        if (wifi.sta.getip() == nil) and (cnt < 20) then 
-            print("Trying Connect to Router, Waiting...")
-            cnt = cnt + 1 
-                 --if cnt%2==1 then gpio.write(0,gpio.LOW);
-                 --else gpio.write(0,gpio.HIGH); end
-        else 
-            tmr.stop(0);
-            print("Soft AP started")
-            print("MAC:"..wifi.ap.getmac().."\r\nIP:"..wifi.ap.getip());
-            print("IP:"..wifi.ap.getip());
-            if (cnt < 20) then 
-                print("Conected to Router\r\nMAC:"..wifi.sta.getmac().."\r\nIP:"..wifi.sta.getip())
-                --script to run, here I'm using our thingspeak example
-                dofile ("dht11_thinkspeak.lua")
-            else 
-                print("Conected to Router Timeout")
-            end
-            --gpio.write(0,gpio.LOW);
-            cnt = nil;cfg=nil;str=nil;ssidTemp=nil;
-            collectgarbage()
-        end 
-    end)
-
-    
+--provides some basic boot up sleep so you can update scripts
+local cnt = 0
+tmr.alarm(0, 1000, 1, function() 
+   if (cnt < 15) then 
+      print("waiting to run...")
+      cnt = cnt + 1 
+   else 
+      tmr.stop(0);
+      print("Running user script...")
+      dofile ("dht11_thinkspeak.lua")
+   end 
+end)
